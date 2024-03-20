519fa20e896a50af6b53990e0487132088546daa
Leon <yurileon@hotmail.com.br>
2024-03-19 23:58:09 -0300

    auth in progress + prisma service config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   test() {
     console.log("Testing");
   }
 
-  signup() {
-    return "I'm signed up";
+  async signup(dto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(dto.password);
+
+    // save user in db
+    const user = await this.prisma?.user.create({
+      data: {
+        email: dto.email,
+        hash: hash,
+      },
+    });
+    return user;
   }
 
   signin() {
